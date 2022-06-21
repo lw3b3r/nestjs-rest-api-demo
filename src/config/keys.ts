277a08b6a51f02f09552435a1ec51c2aa1b98ad5
lw3b3r <daevahush@gmail.com>
2022-06-21 12:14:41 -0400

    Added controller, service, schema and interface for Items to interact with mongo db

---
+++ src/config/keys.ts
@@ -0,0 +1,4 @@
+export default {
+  mongoURI:
+    'mongodb+srv://atlas-admin:uEfexy3sFeaXH2o5@cluster0.2asta.mongodb.net/?retryWrites=true&w=majority',
+};
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,28 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { Item } from './interfaces/item.interface';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class ItemsService {
-  private readonly items: Item[] = [
-    {
-      id: '123543453245',
-      name: 'Item One',
-      qty: 100,
-      description: 'This it item one',
-    },
-    {
-      id: '345345342',
-      name: 'Item Two',
-      qty: 50,
-      description: 'This it item two',
-    },
-  ];
+  constructor(@InjectModel('Item') private readonly itemModel: Model<Item>) {}
 
-  findAll(): Item[] {
-    return this.items;
+  async findAll(): Promise<Item[]> {
+    return await this.itemModel.find();
   }
 
-  findOne(id: string): Item {
-    return this.items.find((item) => item.id === id);
+  async findOne(id: string): Promise<Item> {
+    return await this.itemModel.findOne({ _id: id });
+  }
+
+  async create(item: Item): Promise<Item> {
+    const newItem = new this.itemModel(item);
+    return await newItem.save();
+  }
+
+  async delete(id: string): Promise<Item> {
+    return await this.itemModel.findByIdAndRemove(id);
+  }
+
+  async update(id: string, item: Item): Promise<Item> {
+    return await this.itemModel.findByIdAndUpdate(id, item, { new: true });
   }
 }
